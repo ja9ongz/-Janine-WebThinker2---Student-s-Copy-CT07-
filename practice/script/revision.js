1634bd4a7396301f4714d19bf2133dace5181d50
ja9ongz <ja9ongz@gmail.com>
2024-10-12 09:29:20 +0000

    12 October 2024

---
+++ practice/script/revision.js
@@ -0,0 +1,5 @@
+let validate = () => {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    console.log(username,password)
+};
